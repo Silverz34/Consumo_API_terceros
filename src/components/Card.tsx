19183b5294cdef47a4c75b3659179f66c5ce6eb8
Silverz34 <pradogordillomichael@gmail.com>
2026-03-04 21:32:29 -0600

    conflictos con pasar las imagenes, se usara el formato original de html para imagenes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,21 @@
-import Image from "next/image";
 import { card } from "../../service/interface/personaje";
 
 interface props{
     personaje : card
 }
 export default function PersonajeCard({personaje}:props){
+    const imagenPorDefecto = "/descargar.jpeg"; 
+    const srcImagen = personaje.imageUrl && personaje.imageUrl.trim() !== "" 
+        ? personaje.imageUrl 
+        : imagenPorDefecto
     return(
         <div className="flex flex-col items-center bg-white border border-gray-200
          rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 overflow-hidden">
-            <Image src={personaje.imageUrl || ''}
-                   alt={personaje.name || "personaje"}
-                   width={400}
-                   height={256}
-                   className="w-full h-64 object-cover"/>
+           <img 
+                src={srcImagen}
+                alt={personaje.name || "Personaje de Disney"}
+                className="w-full h-44 object-cover"
+            />
             <div className="p-4 text-center w-full bg-slate-50">
                 <h3 className="text-xl font-blod text-black">
                   {personaje.name}
