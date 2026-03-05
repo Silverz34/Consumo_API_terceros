df1cd9e254a0cdc04ef3517381035341fc263a36
Silverz34 <pradogordillomichael@gmail.com>
2026-03-04 21:42:49 -0600

    sera clicleable para ver otros datos

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 import { card } from "../../service/interface/personaje";
-
+import Link from "next/link";
 interface props{
     personaje : card
 }
@@ -9,18 +9,20 @@ export default function PersonajeCard({personaje}:props){
         ? personaje.imageUrl 
         : imagenPorDefecto
     return(
-        <div className="flex flex-col items-center bg-white border border-gray-200
-         rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 overflow-hidden">
-           <img 
-                src={srcImagen}
-                alt={personaje.name || "Personaje de Disney"}
-                className="w-full h-44 object-cover"
-            />
-            <div className="p-4 text-center w-full bg-slate-50">
-                <h3 className="text-xl font-blod text-black">
-                  {personaje.name}
-                </h3>
+        <Link href={`/personaje/${personaje._id}`} className="block">
+            <div className="flex flex-col items-center bg-white border border-gray-200
+            rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 overflow-hidden">
+            <img 
+                    src={srcImagen}
+                    alt={personaje.name || "Personaje de Disney"}
+                    className="w-full h-44 object-cover"
+                />
+                <div className="p-4 text-center w-full bg-slate-50">
+                    <h3 className="text-xl font-blod text-black">
+                    {personaje.name}
+                    </h3>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
