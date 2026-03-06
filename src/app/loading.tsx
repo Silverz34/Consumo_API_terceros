ed0e1cc949383100887ccb0661e4cece81064d76
Silverz34 <pradogordillomichael@gmail.com>
2026-03-05 23:28:55 -0600

    loading con uso de suspense

---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+
+export default function Loading() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-[60vh]">
+      <video 
+        src="https://v1.pinimg.com/videos/mc/720p/af/f7/14/aff7145be641f28d64ec960cfdfa863c.mp4" 
+        autoPlay   
+        loop       
+        muted     
+        playsInline
+        className="w-48 h-48 object-cover rounded-full shadow-lg mb-6" 
+      />
+      <h2 className="text-2xl font-bold text-white animate-pulse">
+        Cargando..
+      </h2>
+      
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
-"use client"; 
+import { Suspense } from "react";
+import PersonajeCard from "@/components/Card"; 
+import { fetch as fetchpersonaje } from "../../service/service";
+import Loading from "./loading";
 
-import { useState, useEffect } from "react";
-import PersonajeCard from "@/components/Card";
-import { card } from "../../service/interface/personaje"; 
-import { fetch } from "../../service/service";
-
-export default function Home() {
-  const [personajes, setPersonajes] = useState<card[]>([]);
-  useEffect(() => {
-    const cargarPersonajes = async () => {
-      const datos = await fetch();
-      setPersonajes(datos);
-    };
-    cargarPersonajes();
-  }, []);
-
+export default async function Home() {
+  const personajes = await fetchpersonaje();
   return (
+    <Suspense fallback={<Loading/>}>
     <main className="min-h-screen bg-zinc-50 p-8 dark:bg-black">
       <div className="grid grid-cols-3 gap-6">
         {personajes.map((personajeActual) => (
@@ -24,8 +15,8 @@ export default function Home() {
             personaje={personajeActual} 
           />
         ))}
-
       </div>
     </main>
+    </Suspense>
   );
 }
