e657b357c1a775c7233566fdbdf376e8d4bd1801
Silverz34 <pradogordillomichael@gmail.com>
2026-03-05 21:31:42 -0600

    header componente

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+
+
+export default function header(){
+ return(
+   <header className="w-full bg-gray-900 text-white shadow-md">
+      
+      <div className="max-w-7xl mx-auto py-6 ">
+         
+            <div className="pl-5 text-2xl font-bold ">
+               Personajes Disney
+            </div>
+      </div>
+   </header>
+ );
+}
